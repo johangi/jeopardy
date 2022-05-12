5d3274884706646eb395fdea2c3ca39a32d702b2
johangi <johangiaever@gmail.com>
2022-05-12 12:38:07 +0200

    adding files

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 // $("#questionTable td").on("click", function(cell){
 //     mathQ1.classList.add("clicked")
 // })
+
+document.querySelector.on("click", function(){
+    mathQ1.classList.add("clicked")
+})
